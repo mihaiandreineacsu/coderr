2c23a9ea546f136bcd67dd86a9323677d5c1a9c9
Michael Friggemann <m.friggemann@proton.me>
2026-06-24 18:57:26 +0200

    Initial commit: Django + DRF base project

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+"""
+URL configuration for core project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
